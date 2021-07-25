0a7b738812e1f74a0c00d645771617baaa7446f1
gavrileyes <gavriley-es@mail.ru>
2021-07-25 19:46:50 +0300

    complete lesson 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,94 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Message from './components/Message';
+// import MessageClass from './components/MessageClass';
 
-const testMessage = 'Message text'
+
+// const testMessage = 'Message text. Function component';
+// const testMessageClass = 'Message text. Class component';
 
 function App() {
+  // const [showChild, setShowChild] = React.useState(true);
+  const [messageList, setMessageList] = React.useState([]);
+  const [textInput, setTextInput] = React.useState('');
+  const [authorInput, setAuthorInput] = React.useState('');
+
+
+  React.useEffect(() => {
+    if( messageList.length && messageList[messageList.length-1].author ) {
+      setTimeout( () => {
+      alert('Cообщение отправлено!')
+    }, 1500)
+    }
+  }, [messageList])
+
+  // const handleToggleShowChild = () => {
+  //   setShowChild(!showChild);
+  // }
+
+  const bntMessage = (event) => {
+    event.preventDefault();
+    if( textInput.target && authorInput.target && authorInput.target.value !== '') {
+      setMessageList( [
+      ...messageList, 
+      { id: messageList.length+1, 
+        text: textInput.target.value, 
+        author: authorInput.target.value 
+      }
+    ]);
+    } else {
+      alert('Заполните все поля формы');
+    }
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Message message={testMessage}/>
-        
+
+        {/* <button onClick={handleToggleShowChild}>ToggleShowChild</button>
+        {showChild ? <Message message={testMessage} /> : null}
+        {!showChild ? <MessageClass message={testMessageClass} /> : null}
+        */}
+
+        <div className='wrapper-content'>
+          <h2>React - Lesson 2</h2>
+          <form action="#">
+            <input 
+              type="text" 
+              name="text" 
+              id="message" 
+              placeholder="Сообщение"  
+              className='form-input form-input-text'
+              onChange={setTextInput}
+            />
+            <input 
+              type="text" 
+              name="author" 
+              id="author" 
+              placeholder="Автор" 
+              className='form-input'
+              onChange={setAuthorInput} 
+            />
+            <button 
+              type="submit" 
+              className='form-btn'
+              onClick={bntMessage}
+            >
+              Отправить
+            </button>
+          </form>
+          <div className='wrapper-row wrapper-row-title'>
+            <div>id</div>
+            <div>text</div>
+            <div>author</div>
+          </div>
+          {
+            messageList.map(m => <Message key={m.id} message={m} />)
+          }
+        </div>
       </header>
     </div>
   );
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,47 @@
+import React from "react";
+
 function Message(props) {
-  return <div>
-    {props.message}
-  </div>
+  console.log('Start function Message');
+  // const [count, setCount] = React.useState(0);
+
+  //componentDidMount
+  React.useEffect(() => {
+    console.log('function - componentDidMount hook');
+    //componentWillUnmount
+    return () => {
+      console.log('function - componentWillUnmount hook');
+    }
+  }, [])
+  // //componentDidUpdate
+  // React.useEffect(() => {
+  //   console.log('function - componentDidUpdate hook', { count });
+  // }, [count])
+  //componentDidUpdate
+  React.useEffect(() => {
+    console.log('function - componentDidUpdate hook');
+  }, [])
+
+  // const handleClick = () => {
+  //   setCount(currentCount => currentCount + 1);
+  // }
+  // const handleClick2 = React.useCallback(
+  //   () => { setCount(count + 2) }, [count]
+  // );
+
+  console.log('function - Render');
+
+  // <React.Fragment> === <></>
+  return <React.Fragment>
+    <div className='wrapper-row'>
+      <div>{props.message.id}</div>
+      <div>{props.message.text}</div>
+      <div>{props.message.author}</div>
+    </div>
+    {/*<p>Counter = {count}</p>
+    <button onClick={handleClick}>+1</button>
+    <button onClick={handleClick2}>handleClick2 + 2</button> 
+    {props.messageList.map((m, i) => <div key={i}>{m}</div>)} */}
+  </React.Fragment>
 }
 
 export default Message;
